569da1af2c68ce79dfcd93b7aab05564b2bad9fe
Thomas McNish <mcnishthomas@gmail.com>
2022-03-28 08:50:12 -0400

    created Add Player Form component and added it to app.js (frontend only)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
 
 
 class App extends Component {
@@ -63,6 +64,8 @@ class App extends Component {
             removePlayer={this.handleRemovePlayer}
           />
         )}
+
+        <AddPlayerForm />
       </div>
     );
   }
