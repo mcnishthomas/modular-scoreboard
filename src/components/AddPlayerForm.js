a3604660c2e1b9e3a3acba0027021e640fc397e2
Thomas McNish <mcnishthomas@gmail.com>
2022-04-16 17:29:27 -0400

    updated AddPlayerForm.js to handle user input with ref. Converted it back to functional component

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,40 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class AddPlayerForm extends Component {
-    state = {
-        value: ''
-    };
+const AddPlayerForm = ({ addPlayer }) => {
 
-    handleValueChange = (e) => {
-        this.setState({
-            value: e.target.value
-        })
-    }
-
-    handleSubmit = (e) => {
+    let playerInput = React.createRef();
+    let handleSubmit = (e) => {
         e.preventDefault();
-        this.props.addPlayer(this.state.value);
-        this.setState({ value: '' })
-
+        addPlayer(playerInput.current.value);
+        e.currentTarget.reset();
     }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    value={this.state.value}
-                    onChange={this.handleValueChange}
-                    placeholder="Enter a player's name"
-                />
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                ref={playerInput}
+                placeholder="Enter a player's name"
+            />
 
-                <input
-                    type="submit"
-                    value="Add Player"
-                />
-            </form>
-        );
-    }
+            <input
+                type="submit"
+                value="Add Player"
+            />
+        </form>
+    );
 }
 
 export default AddPlayerForm;
